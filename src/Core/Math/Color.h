7e8c6a4cd5fdd5521b9f1baa19798d37e322e0db
Jonne Nauha <jonne@adminotech.com>
2014-10-15 02:39:49 +0300

    Color: Add setters, a lot nicer than spamming all the channels individually in core that uses Color.

--- src/Core/Math/Color.h
+++ src/Core/Math/Color.h
@@ -44,6 +44,16 @@ public:
     {
     }
 
+    void Set(float _r, float _g, float _b)
+    {
+        r = _r; g = _g; b = _b;
+    }
+
+    void Set(float _r, float _g, float _b, float _a)
+    {
+        r = _r; g = _g; b = _b; a = _a;
+    }
+
     bool operator == (const Color& rhs) const
     {
         return r == rhs.r && g == rhs.g && b == rhs.b && a == rhs.a; /**< @todo Use epsilon! */
